5c12bf32d63c78a4ac7a4ccebdc1ab1fb2be5faf
Jorge Galarza <jorge2000cruz@gmail.com>
2022-07-15 09:45:55 -0500

    unit testing: Example 1 and 2 from IOET exercise

--- test/test.js
+++ test/test.js
@@ -1,4 +1,9 @@
 const EMPLOYEE_DATA_FILENAME = "./data/employee-entry-control.txt";
+const EMPLOYEE_DATA_FILENAME_IOET_EXAMPLE1 =
+  "./data/employee-entry-control-example1.txt";
+const EMPLOYEE_DATA_FILENAME_IOET_EXAMPLE2 =
+  "./data/employee-entry-control-example2.txt";
+
 const {
   getResult,
   removeUnnecessaryCharacters,
@@ -13,6 +18,15 @@ test("getResult function should return a string with the correct output", () => 
   const expectedResult =
     "ASTRID-RENE: 2\nASTRID-ANDRES: 3\nASTRID-JORGE: 2\nASTRID-CARLOS: 2\nRENE-ANDRES: 2\nRENE-JORGE: 2\nRENE-CARLOS: 2\nANDRES-JORGE: 2\nANDRES-CARLOS: 2\nJORGE-CARLOS: 1\n";
   expect(getResult(EMPLOYEE_DATA_FILENAME)).toBe(expectedResult);
+});
+
+test("getResult function should return a string with the correct output: IOET Example 1", () => {
+  const expectedResult = "RENE-ASTRID: 2\nRENE-ANDRES: 2\nASTRID-ANDRES: 3\n";
+  expect(getResult(EMPLOYEE_DATA_FILENAME_IOET_EXAMPLE1)).toBe(expectedResult);
+});
+test("getResult function should return a string with the correct output: IOET Example 2", () => {
+  const expectedResult = "RENE-ASTRID: 3\n";
+  expect(getResult(EMPLOYEE_DATA_FILENAME_IOET_EXAMPLE2)).toBe(expectedResult);
 });
 
 // ------------------------ Testing removeUnnecessaryCharacters function --------------------------- //
